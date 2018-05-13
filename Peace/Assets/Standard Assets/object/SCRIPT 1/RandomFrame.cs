09ae39553dd2c5ca51866be2e8a1460efc63216c
Kefa95 <Kefa95@ukr.net>
2018-05-13 13:44:43 +0400

    56565
    75

---
+++ Peace/Assets/Standard Assets/object/SCRIPT 1/RandomFrame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFrame : MonoBehaviour 
+{
+	void Start () 
+	{
+        Random.seed = System.DateTime.Now.Millisecond;
+        var animator = GetComponent<Animator>();
+		AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+		animator.Play (state.fullPathHash, 0, Random.Range (0f, 1f));
+	}
+
+	void Update () 
+	{
+		
+	}
+}
